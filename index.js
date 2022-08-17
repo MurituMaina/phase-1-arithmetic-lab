527976939c1c9c637b15438ef1a5d82a0d70741f
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-17 23:08:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+//Test 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+// let multiply = 31 * 2;
+
+multiply;
+//Test 2
+let random = (Math.ceil(Math.random())*10);
+console.log(random)
+//Test 3
+let num4 = 18;
+let num3 = 7;
+let mod = num4 % num3;
+mod;
+// Test 4
+let max = (2,4,6,8,10,12,14,16,18,20)
+let bigNumber = Math.max(max);
+bigNumber;
